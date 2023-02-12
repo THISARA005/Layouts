bb4b3078c25e8d8acc9390380569a53bdb08e7ba
B.A Thisara Dilshan <88025945+THISARA005@users.noreply.github.com>
2023-02-12 18:06:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/practical3/Task05.java
@@ -0,0 +1,14 @@
+package com.example.practical3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Task05 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task05);
+    }
+}
